7e69c6d3e78e1433f270e8353d740fea78badd46
Dan Connolly <dckc@madmode.com>
2024-05-20 18:23:30 -0500

    style: lint dao.contract.js whitespace

--- contract/src/dao.contract.js
+++ contract/src/dao.contract.js
@@ -9,265 +9,272 @@ import { atomicRearrange } from '@agoric/zoe/src/contractSupport/atomicTransfer.
 import { makeMyAddressNameAdminKit } from '@agoric/vats/src/core/utils';
 import { makeOnewayPriceAuthorityKit } from '@agoric/zoe/src/contractSupport';
 import {
-    defineERecorderKit,
-    prepareRecorderKitMakers,
-    provideAll
+  defineERecorderKit,
+  prepareRecorderKitMakers,
+  provideAll,
 } from '@agoric/zoe/src/contractSupport/index.js';
 import { handleParamGovernance } from '@agoric/governance/src/contractHelper.js';
 import { ParamTypes } from '@agoric/governance/src/constants.js';
-  
 
-
-// TODO: use shape 
+// TODO: use shape
 const VoteShape = M.recordOf(M.string(), {
- voteAmount: AmountShape,
+  voteAmount: AmountShape,
 });
 
-
 export const terms = harden({
-    DaoTerms:  {
-        DaoToken: AmountShape,
-        Membership: AmountShape,
-}});
+  DaoTerms: {
+    DaoToken: AmountShape,
+    Membership: AmountShape,
+  },
+});
 
+export const start = async (zcf, privateArgs, baggage) => {
+  //dao proposal abstractions
+  const proposals = new Map(); // track proposals by UIDs
+  let nextProposalId = 1n; // naive generate proposal IDs
 
-export const start = async (zcf , privateArgs, baggage) => {
+  // create a mints
+  const daoTokenMint = await zcf.makeZCFMint('DaoToken', AssetKind.NAT);
+  const { brand: daoTokenBrand, issuer: daoTokenIssuer } =
+    daoTokenMint.getIssuerRecord();
+  console.log(daoTokenMint);
 
-    //dao proposal abstractions 
-    const proposals = new Map(); // track proposals by UIDs
-    let nextProposalId = 1n; // naive generate proposal IDs
+  const membershipMint = await zcf.makeZCFMint(
+    'Membership',
+    AssetKind.COPY_BAG,
+  );
+  const { brand: membershipBrand, issuer: membershipIssuer } =
+    membershipMint.getIssuerRecord();
+  console.log(membershipMint);
 
-    // create a mints
-    const daoTokenMint = await zcf.makeZCFMint('DaoToken', AssetKind.NAT);
-    const { brand: daoTokenBrand, issuer: daoTokenIssuer } = daoTokenMint.getIssuerRecord();
-    console.log(daoTokenMint)
-    
-    const membershipMint = await zcf.makeZCFMint('Membership', AssetKind.COPY_BAG);
-    const { brand: membershipBrand, issuer: membershipIssuer } = membershipMint.getIssuerRecord();
-    console.log(membershipMint)
+  // mint initial DAO tokens
+  const initialDaoTokens = 100n;
+  const { zcfSeat: initialDaoSeat } = zcf.makeEmptySeatKit();
 
-    // mint initial DAO tokens
-    const initialDaoTokens = 100n;
-    const { zcfSeat: initialDaoSeat } = zcf.makeEmptySeatKit();
+  console.log(initialDaoSeat);
 
-    console.log(initialDaoSeat)
+  const toMint = {
+    DaoTokens: {
+      brand: daoTokenBrand,
+      value: initialDaoTokens,
+    },
+  };
 
-    const toMint = {
-        DaoTokens: {
-            brand: daoTokenBrand,
-            value: initialDaoTokens,
-        },
+  daoTokenMint.mintGains(toMint);
+
+  //join proposal
+  const joinProposalShape = harden({
+    give: {},
+    want: {
+      NewMembership: {
+        brand: membershipBrand,
+        value: M.bag(),
+      },
+      DaoTokens: {
+        brand: daoTokenBrand,
+        value: M.nat(),
+      },
+    },
+    exit: M.any(),
+  });
+
+  //vote proposal
+  const voteProposalShape = harden({
+    give: {
+      CurrentMembership: {
+        brand: membershipBrand,
+        value: M.bag(),
+      },
+      Votes: { brand: daoTokenBrand, value: M.gte(10n) },
+    },
+    want: {
+      NewMembership: {
+        brand: membershipBrand,
+        value: M.bag(),
+      },
+    },
+    exit: M.any(),
+  });
+
+  //create DAO proposal... proposal lol
+  const createProposalProposalShape = harden({
+    give: {},
+    want: {},
+    exit: M.any(),
+  });
+
+  const { makeRecorderKit } = prepareRecorderKitMakers(
+    baggage,
+    privateArgs.marshaller,
+  );
+
+  // initialDaoSeat.exit(); // close the seat after minting?
+
+  // allow users to join and receive a Membership NFT
+  const joinDao = async joiningmMemberSeat => {
+    /** @type {[string, bigint][]} */
+    const choices = [['MembershipCard1', 1n]];
+
+    const choiceBag = makeCopyBag(choices);
+    const membershipToMint = {
+      NewMembership: {
+        brand: membershipBrand,
+        value: choiceBag,
+      },
     };
 
-    daoTokenMint.mintGains(toMint);
+    const daoTokenToMint = {
+      DaoTokens: {
+        brand: daoTokenBrand,
+        value: 10n,
+      },
+    };
 
-    //join proposal
-    const joinProposalShape = harden({
-        give: { },
-        want: { 
-            NewMembership: { 
-                brand: membershipBrand, 
-                value: M.bag() 
-            }, 
-            DaoTokens: { 
-                brand: daoTokenBrand, 
-                value: M.nat()
-            }, 
-        },
-        exit: M.any(), 
-    });
+    const { give, want } = joiningmMemberSeat.getProposal();
 
-    //vote proposal
-    const voteProposalShape = harden({
-        give: { 
-            CurrentMembership: {
-                brand: membershipBrand, 
-                value: M.bag(), 
-            },
-            Votes: { brand: daoTokenBrand, value: M.gte(10n) }
-        },
-        want: {
-            NewMembership: {
-                brand: membershipBrand, 
-                value: M.bag(), 
-            }
-        },
-        exit: M.any(),
-    })
+    const daoTokenSeat = daoTokenMint.mintGains(daoTokenToMint); //DAO tokens on joining
+    const membershipSeat = membershipMint.mintGains(membershipToMint); //membership card on join int
 
-    //create DAO proposal... proposal lol
-    const createProposalProposalShape = harden({
-        give: { 
+    const feeAmount = AmountMath.make(daoTokenBrand, 10n);
+    const membershipAmount = AmountMath.make(membershipBrand, choiceBag);
 
-        },
-        want: {
-
-        },
-        exit: M.any(),
-    })
-    
-    const { makeRecorderKit } = prepareRecorderKitMakers(
-        baggage,
-        privateArgs.marshaller,
+    atomicRearrange(
+      zcf,
+      harden([
+        // send from joiningMember to initialDaoSeat
+        // [joiningmMemberSeat, initialDaoSeat, give ],
+        //send from
+        [daoTokenSeat, joiningmMemberSeat, { DaoTokens: feeAmount }],
+        [
+          membershipSeat,
+          joiningmMemberSeat,
+          { NewMembership: membershipAmount },
+        ],
+        // [daoTokenSeat, joiningmMemberSeat, want],
+      ]),
     );
-    
-    // initialDaoSeat.exit(); // close the seat after minting? 
 
-    // allow users to join and receive a Membership NFT
-    const joinDao = async (joiningmMemberSeat) => {
+    joiningmMemberSeat.exit(true); // close the seat after minting
+    return 'Membership NFT granted with DAO tokens.';
+  };
 
-        /** @type {[string, bigint][]} */
-        const choices = [
-            ['MembershipCard1', 1n],
-        ];
+  const makeJoinInvitation = () =>
+    zcf.makeInvitation(
+      joinDao,
+      'Simple Dao Join',
+      undefined,
+      joinProposalShape,
+    );
 
-        const choiceBag = makeCopyBag(choices);
-        const membershipToMint = {
-            NewMembership: {
-                brand: membershipBrand,
-                value: choiceBag,
-            },
-        };
+  // allow voTing if a member has an NFT and gives DAO tokens
+  const vote = async (voterSeat, { proposalId, voteFor }) => {
+    const membershipHeld = voterSeat.getAmountAllocated(
+      'Membership',
+      membershipBrand,
+    );
+    const daoTokensGiven = voterSeat.getAmountAllocated('Votes', daoTokenBrand);
+    const proposal = proposals.get(proposalId);
+    const { give, want } = voterSeat.getProposal();
 
-        const daoTokenToMint = {
-            DaoTokens: {
-                brand: daoTokenBrand,
-                value: 10n,
-            },
-        };
+    if (!membershipHeld || !daoTokensGiven) {
+      throw new Error('Must hold a Membership NFT and DAO tokens to vote.');
+    }
 
-        const { give, want } = joiningmMemberSeat.getProposal();
+    if (!proposal) {
+      throw new Error('Proposal not found.');
+    }
 
-        const daoTokenSeat = daoTokenMint.mintGains(daoTokenToMint); //DAO tokens on joining
-        const membershipSeat = membershipMint.mintGains(membershipToMint); //membership card on join int
+    if (voteFor) {
+      proposal.votesFor += daoTokensGiven.value;
+    } else {
+      proposal.votesAgainst += daoTokensGiven.value;
+    }
 
-        const feeAmount = AmountMath.make(daoTokenBrand, 10n); 
-        const membershipAmount = AmountMath.make(membershipBrand, choiceBag);
+    /** @type {[string, bigint][]} */
+    const choices = [['MembershipCard2', 1n]];
 
-
-        atomicRearrange(
-            zcf,
-            harden([
-                // send from joiningMember to initialDaoSeat
-                // [joiningmMemberSeat, initialDaoSeat, give ],
-                //send from 
-                [daoTokenSeat, joiningmMemberSeat, { DaoTokens: feeAmount }],
-                [membershipSeat, joiningmMemberSeat, { NewMembership: membershipAmount }]
-                // [daoTokenSeat, joiningmMemberSeat, want],
-                
-            ]),
-        );
-
-        joiningmMemberSeat.exit(true); // close the seat after minting
-        return 'Membership NFT granted with DAO tokens.';
+    const choiceBag = makeCopyBag(choices);
+    const membershipToMint = {
+      NewMembership: {
+        brand: membershipBrand,
+        value: choiceBag,
+      },
     };
 
-    const makeJoinInvitation = () => zcf.makeInvitation(joinDao, "Simple Dao Join", undefined, joinProposalShape);
+    // create seat for swapping from proceed to voterSeat, what happens to this seats purse when its done?
+    // does it burn, because nothing happens with the purse? if so we probably want to send to the DAO seat?
+    const proceeds = membershipMint.mintGains(membershipToMint);
 
-    // allow voTing if a member has an NFT and gives DAO tokens
-    const vote = async (voterSeat, { proposalId, voteFor }) => {
-        const membershipHeld = voterSeat.getAmountAllocated('Membership', membershipBrand);
-        const daoTokensGiven = voterSeat.getAmountAllocated('Votes', daoTokenBrand);
-        const proposal = proposals.get(proposalId);
-        const { give, want } = voterSeat.getProposal();
+    atomicRearrange(
+      zcf,
+      harden([
+        // membership token from voter to membershipMint
+        // TODO: split give to two seats?
+        [voterSeat, initialDaoSeat, give],
+        [proceeds, voterSeat, want],
+      ]),
+    );
+    voterSeat.exit(true);
+    return `Vote accepted. ${daoTokensGiven.value} DaoTokens spent.`;
+  };
 
-        if (!membershipHeld || !daoTokensGiven) {
-            throw new Error("Must hold a Membership NFT and DAO tokens to vote.");
-        }
+  const makeVoteInvitation = () =>
+    zcf.makeInvitation(vote, 'Simple Dao Vote', undefined, voteProposalShape);
 
-        if (!proposal) {
-            throw new Error("Proposal not found.");
-        }
+  //create proposal func
+  const createProposal = async (creatorSeat, title, details) => {
+    const id = BigInt(nextProposalId++);
+    proposals.set(id, { id, title, details, votesFor: 0n, votesAgainst: 0n });
+    console.log('Preparing to record proposal...');
 
-        if (voteFor) {
-            proposal.votesFor += daoTokensGiven.value;
-        } else {
-            proposal.votesAgainst += daoTokensGiven.value;
-        }
+    try {
+      // cnvert Map to Array for storage
+      const proposalsArray = Array.from(proposals.values()).map(proposal => ({
+        id: proposal.id,
+        title: proposal.title,
+        details: proposal.details,
+        votesFor: proposal.votesFor.toString(),
+        votesAgainst: proposal.votesAgainst.toString(),
+      }));
 
-        /** @type {[string, bigint][]} */
-        const choices = [
-            ['MembershipCard2', 1n],
-        ];
+      console.log('Array of proposals prepared:', proposalsArray);
 
-        const choiceBag = makeCopyBag(choices);
-        const membershipToMint = {
-            NewMembership: {
-                brand: membershipBrand,
-                value: choiceBag,
-            },
-        };
+      // const proposalNode = await E(privateArgs.storageNode).makeChildNode(`proposal`);
+      const recorderKit = makeRecorderKit(
+        privateArgs.storageNode,
+        // proposalNode,
+        /** @type {import('@agoric/zoe/src/contractSupport/recorder.js').TypedMatcher<MetricsNotification>} */ (
+          M.any()
+        ),
+      );
 
-        // create seat for swapping from proceed to voterSeat, what happens to this seats purse when its done? 
-        // does it burn, because nothing happens with the purse? if so we probably want to send to the DAO seat?
-        const proceeds = membershipMint.mintGains(membershipToMint);
+      console.log('recorder Kit initialized');
+      await recorderKit.recorder.write(proposalsArray);
+      console.log('Proposals recorded successfully in storage.');
+    } catch (error) {
+      console.error('Failed to record proposals:', error);
+      throw new Error('Failed to save proposals.');
+    }
 
-        atomicRearrange(
-            zcf,
-            harden([
-                // membership token from voter to membershipMint
-                // TODO: split give to two seats?
-                [voterSeat, initialDaoSeat, give ],
-                [proceeds, voterSeat, want],
-                
-            ]),
-        );
-        voterSeat.exit(true)
-        return `Vote accepted. ${daoTokensGiven.value} DaoTokens spent.`;
-    };
+    creatorSeat.exit(true);
+    return id;
+  };
 
-    const makeVoteInvitation = () => zcf.makeInvitation(vote, "Simple Dao Vote", undefined, voteProposalShape);
+  const createProposalInvitation = () =>
+    zcf.makeInvitation(
+      createProposal,
+      'Create DAO Proposal',
+      undefined,
+      createProposalProposalShape,
+    );
 
+  const publicFacet = Far('Dao Public Facet', {
+    makeJoinInvitation,
+    makeVoteInvitation,
+    createProposalInvitation,
+  });
 
-    //create proposal func
-    const createProposal = async (creatorSeat, title, details) => {
-        const id = BigInt(nextProposalId++);
-        proposals.set(id, { id, title, details, votesFor: 0n, votesAgainst: 0n });
-        console.log("Preparing to record proposal...");
-    
-        try {
-            // cnvert Map to Array for storage
-            const proposalsArray = Array.from(proposals.values()).map(proposal => ({
-                id: proposal.id,
-                title: proposal.title,
-                details: proposal.details,
-                votesFor: proposal.votesFor.toString(),
-                votesAgainst: proposal.votesAgainst.toString()
-            }));
-    
-            console.log("Array of proposals prepared:", proposalsArray);
-    
-            // const proposalNode = await E(privateArgs.storageNode).makeChildNode(`proposal`);
-            const recorderKit = makeRecorderKit(
-                privateArgs.storageNode,
-                // proposalNode,
-                /** @type {import('@agoric/zoe/src/contractSupport/recorder.js').TypedMatcher<MetricsNotification>} */ (
-                  M.any()
-                ),
-            )
-    
-            console.log("recorder Kit initialized");
-            await recorderKit.recorder.write(proposalsArray);
-            console.log("Proposals recorded successfully in storage.");
-    
-        } catch (error) {
-            console.error("Failed to record proposals:", error);
-            throw new Error("Failed to save proposals.");
-        }
-    
-        creatorSeat.exit(true);
-        return id;
-    };
+  return harden({ publicFacet });
+};
 
-    const createProposalInvitation = () => zcf.makeInvitation(createProposal, "Create DAO Proposal", undefined, createProposalProposalShape);
-
-    const publicFacet = Far('Dao Public Facet', {
-        makeJoinInvitation,
-        makeVoteInvitation,
-        createProposalInvitation
-    });
-
-    return harden({ publicFacet });
-}
-
-harden(start)
+harden(start);
